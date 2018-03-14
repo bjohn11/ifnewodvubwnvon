ab89209b60c9aed5a7566e5c02b6acdfdf38520c
bjohn11 <bjohn11@mail.depaul.edu>
2018-03-14 05:10:36 -0400

    new style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201021206) do
+ActiveRecord::Schema.define(version: 20180314085548) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170201021206) do
   create_table "typos", force: :cascade do |t|
     t.string   "name"
     t.string   "stuff"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
